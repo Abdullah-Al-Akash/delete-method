ff2d97d14450ceac2680e495ee0dc3ff50b706a3
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-02-10 16:07:24 +0600

    Update

--- app.js
+++ app.js
@@ -24,3 +24,42 @@ function deleteValidation() {
 deleteBtn.addEventListener("click", function () {
         name.style.display = "none";
 })
+
+// Added New Sobji:
+const addSobji = document.getElementById('sobji-input');
+const sobjiAdd = document.getElementById('sobji-add');
+const ul = document.getElementById('ul');
+
+addSobji.addEventListener("change", function (e) {
+        sobjiValidation(e.target.value);
+
+        sobjiAdd.addEventListener("click", function () {
+                const li = document.createElement('li');
+                li.innerHTML = `${e.target.value} <button class="btn">❌</button>`;
+                ul.appendChild(li);
+                sobjiAdd.setAttribute("disabled", true);
+        })
+})
+
+addSobji.addEventListener("keyup", function (e) {
+        sobjiValidation(e.target.value);
+
+        sobjiAdd.addEventListener("click", function () {
+                const li = document.createElement('li');
+                li.innerHTML = `${e.target.value} <button class="btn">❌</button>`;
+                ul.appendChild(li);
+                sobjiAdd.setAttribute("disabled", true);
+        })
+})
+
+// Validation for Add Sobji
+function sobjiValidation(parameter) {
+        if (parameter.length > 0) {
+                sobjiAdd.removeAttribute("disabled");
+        }
+        else {
+                sobjiAdd.setAttribute("disabled", true);
+        }
+}
+
+// Add Sobji Via Add Button:
